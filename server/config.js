55334392818dd9c1c15d2c2ea146da65b01d4370
Pinakin <pinakin.sculptsoft@gmail.com>
2021-03-01 19:12:02 +0530

    Init commit

---
+++ server/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  twilio: {
+    // accountSid: process.env.TWILIO_ACCOUNT_SID,
+    // apiKey: process.env.TWILIO_API_KEY,
+    // apiSecret: process.env.TWILIO_API_SECRET
+
+    accountSid:"AC744c3a9c41091bb34880cc286d766a61",
+    apiKey:"SKaea227057f749f3cd7405902b6408149",
+    apiSecret:"FdeHad67rHNnei7GQ5H4b8CscTkeF5h3"
+
+    // MHC App
+    // accountSid: "AC79eab16aca20635f543dda541b259b1c",
+		// apiKey: "SKbf4f7a7eee2868e3c2c0bad4355d883f",
+		// apiSecret: "tUzITiwxWF9eFaE56GYyAfGTQQpGZnYh"
+
+  }
+};
---
+++ server/index.js
@@ -0,0 +1,60 @@
+const config = require('./config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
+const { videoToken } = require('./tokens');
+var cors = require('cors');
+
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(pino);
+
+app.use(cors(
+	{
+		origin: '*'
+	}));
+
+  // const proxy = require("http-proxy-middleware");
+// module.exports = function(app) {
+//   app.use(proxy("/video/token", { // https://github.com/chimurai/http-proxy-middleware
+//     target: "http://localhost:3001",
+//     secure: false
+//   }));
+// };
+
+const sendTokenResponse = (token, res) => {
+  res.set('Content-Type', 'application/json');
+  res.send(
+    JSON.stringify({
+      token: token.toJwt()
+    })
+  );
+};
+
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
+
+app.get('/video/token', (req, res) => {
+  const identity = req.query.identity;
+  const room = req.query.room;
+  const token = videoToken(identity, room, config);
+  sendTokenResponse(token, res);
+
+});
+app.post('/video/token', (req, res) => {
+  const identity = req.body.identity;
+  const room = req.body.room;
+  console.log(identity, 'identity');
+  console.log(room, 'room');
+  const token = videoToken(identity, room, config);
+  sendTokenResponse(token, res);
+});
+
+app.listen(3001, () =>
+  console.log('Express server is running on localhost:3001')
+);
